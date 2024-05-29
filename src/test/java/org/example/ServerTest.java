48be85913be3a2a646edaf7e21494fd35f3beb1f
Iurii <yurasik96@inbox.ru>
2024-05-29 16:20:49 +0500

    3.0

--- src/test/java/org/example/ServerTest.java
+++ src/test/java/org/example/ServerTest.java
@@ -12,7 +12,7 @@ import java.io.*;
 class ServerTest {
     @Test
     void choosePort_test() throws IOException {
-        File fileSettings = new File("C:/Users/Юрий/IdeaProjects/diplom_2/server/src/main/resources/settings_test.txt");
+        File fileSettings = new File("settings_test.txt");
         //
         String actual = Server.choosePort(fileSettings);
         String expected = "Current port is: 26";
